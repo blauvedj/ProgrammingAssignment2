fe2ce8a3c3bab847327b411afff7a3c8455b8125
blauvedj <david.j.blauvelt@nga.mil>
2014-10-24 13:25:45 -0400

    Applied changes test commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions store a value in a 'superfunction' then perform an operation 
+## in this case the inverse (solve()) function.
 
-## Write a short comment describing this function
+## This first function creates a special 'matrix' which sets 
+## the value of the matrix and gets the value of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set<-function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+
+## The second function calculates the inverse of the matrix created in the first
+## function. The solve() command creates the inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
